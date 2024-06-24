79a2385e315546e1caf600d92166a198a53c6881
mccampero <mccampero2@gmail.com>
2024-06-24 15:27:35 -0400

    index created

---
+++ models/blogs.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+
+const blogSchema = new mongoose.Schema({
+  name: String,
+  isReadyToCreate: Boolean,
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
--- server.js
+++ server.js
@@ -11,9 +11,39 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+const Blog = require("./models/blogs.js");
+
+//middleware 
+app.use(express.urlencoded({ extended: false }));
 // GET /
 app.get("/", async (req, res) => {
-    res.send("hello, friend!");
+    res.render("index.ejs");
+      });
+
+// GET /blogs
+
+
+app.get("/blogs", async (req, res) => {
+    const allBlogs = await Blog.find();
+    res.render("blogs/index.ejs", { blogs: allBlogs });
+  });
+  
+app.get("/blogs/new",  (req, res) => {
+    res.render("blogs/new.ejs");
+  });
+
+  
+  
+
+  // POST /fruits
+  app.post("/blogs", async (req, res) => {
+    if (req.body.isReadyToCreate === "on") {
+      req.body.isReadyToCreate = true;
+    } else {
+      req.body.isReadyToCreate = false;
+    }
+    await Blog.create(req.body);
+    res.redirect("/blogs");
   });
   
 app.listen(3000, () => {
